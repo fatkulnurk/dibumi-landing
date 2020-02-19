4c9eaa685755d5e3b14a778b6f17ae335133e7a8
fatkulnurk <fatkulnurk@gmail.com>
2020-02-19 16:57:15 +0700

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+</style>
+
+<!--<Nav {segment}/>-->
+
+<main>
+	<slot></slot>
+</main>
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>
+			Copyright &copy; 2020  Dibumi.Com <br>
+			Dibuat dengan <span class="has-text-danger has-text-weight-bold"><i class="fas fa-heart"></i></span> di Indonesia (Lamongan & Surabaya)
+		</p>
+	</div>
+</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,71 @@
+<style xmlns:svelte="http://www.w3.org/1999/html">
+</style>
+
+<svelte:head>
+	<title>Dibumi.com</title>
+</svelte:head>
+<section class="hero is-medium is-fullwidth is-primary is-bold">
+	<div class="hero-body has-text-centered">
+		<div class="container">
+			<h1 class="title is-1 is-uppercase">
+				Dibumi.Com
+			</h1>
+			<h2 class="subtitle is-3">
+				Menyelesaikan persoalan di indonesia lewat berbagai macam produk dan layanan
+			</h2>
+		</div>
+	</div>
+</section>
+<section class="section has-text-centered">
+	<div class="container">
+		<h3 class="title">Produk Kami</h3>
+		<p class="subtitle">
+			Beragam produk dan layanan dibuat oleh kami
+		</p>
+	</div>
+	<br>
+	<div class="container">
+		<div class="columns">
+			<a class="column is-4" href="https://bayarcepat.com" target="_blank">
+				<div class="box">
+					<span class="has-text-primary">
+					  <i class="fas fa-coins fa-4x"></i>
+					</span>
+					<h4 class="title is-4">BayarCepat</h4>
+					<p class="subtitle">Layanan keuangan yang membantu hidupmu menjadi lebih sempurna</p>
+				</div>
+			</a>
+			<a class="column is-4" href="https://toko.dibumi.com" target="_blank">
+				<div class="box">
+					<span class="has-text-primary">
+					  <i class="fas fa-shopping-bag fa-4x"></i>
+					</span>
+					<h4 class="title is-4">Toko Dibumi</h4>
+					<p class="subtitle">Buat toko online pribadimu dalam hitungan detik saja</p>
+				</div>
+			</a>
+			<a class="column is-4" target="_blank">
+				<div class="box">
+					<span class="has-text-primary">
+					  <i class="fas fa-piggy-bank fa-4x"></i>
+					</span>
+					<h4 class="title is-4">Uranus Equity</h4>
+					<p class="subtitle">Layanan Urun Dana yang fokus untuk ekspansi bisnis UKM</p>
+				</div>
+			</a>
+		</div>
+		<div class="columns">
+			<a class="column is-4 is-offset-4" target="_blank">
+				<div class="box">
+					<h4 class="title is-4">Masih Berjuang</h4>
+					<p class="subtitle">Perjuangan Kami belum selesai, tunggu produk selanjutnya</p>
+				</div>
+			</a>
+		</div>
+	</div>
+</section>
+
+	<script>
+		/* ---- particles.js config ---- */
+
+	</script>
